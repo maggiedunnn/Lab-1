93c769ff0d3d0cd6bc45d71fef87fa6a4ce7e20c
Maggie Dunn <116841706+maggiedunnn@users.noreply.github.com>
2022-11-01 12:50:11 -0400

    Add files via upload

---
+++ L11.py
@@ -0,0 +1,42 @@
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+#cody brown
+print("How\nare\you?")
+How
+are\you?
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today" + "is" + "Tuesday")
+TodayisTuesday
+print("Today"+ "is"+ "Tuesday")
+TodayisTuesday
+print("Today"+"is"+"Tuesday")
+1+2*3
+SyntaxError: multiple statements found while compiling a single statement
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+10%1
+0
+2%1
+0
+6%2
+0
+2*(3**4)+1
+163
+print("2+2=", 2+2)
+2+2= 4
+print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
+#cody brown
